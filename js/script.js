fb691016d44fad3c1226c7879f63143d9968b406
Igor Romanchuk <defhn900@gmail.com>
2021-01-19 21:27:40 +0300

    Igor_Romacnhyk-hw_1--GIT

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
    $('.quote__slider').slick({
        arrows:false,
        dots:true,
+       adaptiveHeight:true,
+       speed:800,
    });
    
    $('.header__burger').click(function(event) {
